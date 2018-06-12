df393f6afb2f251155a7d4c9f7f378a50d3265ad
duanzihuang <huangwei@itcast.cn>
2018-06-12 21:19:22 +0800

    08-完成了分类页面

--- src/utils/https.js
+++ src/utils/https.js
@@ -10,3 +10,21 @@ export default params=>{
       header:params.header || {}
   })
 }
+
+//导出一个https中GET请求的方法
+export const httpsGet = (url,data={},header={}) => {
+  // return new Promise((resolve,reject)=>{
+  //   wepy.request({
+  //     url: `${API_HOST}${url}`, //开发者服务器接口地址",
+  //     data ,
+  //     success: resolve,
+  //     fail: reject
+  //   })
+  // })
+
+  return wepy.request({
+    url: `${API_HOST}${url}`, //开发者服务器接口地址",
+    data,
+    header
+  })
+}
